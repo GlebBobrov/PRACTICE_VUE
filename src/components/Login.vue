e7368c538fa8567804782614b586634c9197b966
Gleb Bobrov <102213995+GlebBobrov@users.noreply.github.com>
2026-02-05 14:12:19 +0200

    DAY 1

---
+++ src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login">
+    Hello, World!
+  </div>
+</template>
+
+
+<script setup>
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './assets/css/style.css'
+import App from './App.vue'
+
+import './assets/css/style.css'
+
+createApp(App).mount('#app')
